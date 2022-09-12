8d1edac97a2788ee3b883d41257359aa917c9370
pho.tran <tranthanhpho2011@gmail.com>
2022-09-12 21:47:10 +0700

    convert jason to struct

--- elixir/lib/dartj.ex
+++ elixir/lib/dartj.ex
@@ -9,7 +9,7 @@ defmodule Dartj do
   ## Examples
 
       iex -S mix
-      iex> Dartj.decode()
+      iex> Dartj.decode_with_poison()
       [
         %Response{
           address: %Address{
@@ -53,8 +53,45 @@ defmodule Dartj do
         }
       ]
   """
-  def decode do
-    json = ~s([{"id":1,"name":"Leanne Graham","username":"Bret","email":"Sincere@april.biz","address":{"street":"Kulas Light","suite":"Apt. 556","city":"Gwenborough","zipcode":"92998-3874","geo":{"lat":"-37.3159","lng":"81.1496"}},"phone":"1-770-736-8031 x56442","website":"hildegard.org","company":{"name":"Romaguera-Crona","catchPhrase":"Multi-layered client-server neural-net","bs":"harness real-time e-markets"}},{"id":2,"name":"Ervin Howell","username":"Antonette","email":"Shanna@melissa.tv","address":{"street":"Victor Plains","suite":"Suite 879","city":"Wisokyburgh","zipcode":"90566-7771","geo":{"lat":"-43.9509","lng":"-34.4618"}},"phone":"010-692-6593 x09125","website":"anastasia.net","company":{"name":"Deckow-Crist","catchPhrase":"Proactive didactic contingency","bs":"synergize scalable supply-chains"}}])
+  def decode_with_poison do
+    json =
+      ~s([{"id":1,"name":"Leanne Graham","username":"Bret","email":"Sincere@april.biz","address":{"street":"Kulas Light","suite":"Apt. 556","city":"Gwenborough","zipcode":"92998-3874","geo":{"lat":"-37.3159","lng":"81.1496"}},"phone":"1-770-736-8031 x56442","website":"hildegard.org","company":{"name":"Romaguera-Crona","catchPhrase":"Multi-layered client-server neural-net","bs":"harness real-time e-markets"}},{"id":2,"name":"Ervin Howell","username":"Antonette","email":"Shanna@melissa.tv","address":{"street":"Victor Plains","suite":"Suite 879","city":"Wisokyburgh","zipcode":"90566-7771","geo":{"lat":"-43.9509","lng":"-34.4618"}},"phone":"010-692-6593 x09125","website":"anastasia.net","company":{"name":"Deckow-Crist","catchPhrase":"Proactive didactic contingency","bs":"synergize scalable supply-chains"}}])
+
     Poison.decode!(json, as: [%Response{address: %Address{geo: %Geo{}}, company: %Company{}}])
   end
+
+
+  # Dartj.decode_with_jason()
+  def decode_with_jason do
+    json =
+      ~s([{"id":1,"name":"Leanne Graham","username":"Bret","email":"Sincere@april.biz","address":{"street":"Kulas Light","suite":"Apt. 556","city":"Gwenborough","zipcode":"92998-3874","geo":{"lat":"-37.3159","lng":"81.1496"}},"phone":"1-770-736-8031 x56442","website":"hildegard.org","company":{"name":"Romaguera-Crona","catchPhrase":"Multi-layered client-server neural-net","bs":"harness real-time e-markets"}},{"id":2,"name":"Ervin Howell","username":"Antonette","email":"Shanna@melissa.tv","address":{"street":"Victor Plains","suite":"Suite 879","city":"Wisokyburgh","zipcode":"90566-7771","geo":{"lat":"-43.9509","lng":"-34.4618"}},"phone":"010-692-6593 x09125","website":"anastasia.net","company":{"name":"Deckow-Crist","catchPhrase":"Proactive didactic contingency","bs":"synergize scalable supply-chains"}}])
+
+    json |> Jason.decode!() |> to_srtuct(nil)
+  end
+
+  def to_srtuct(json, module) when is_list(json) do
+    json 
+    |> Enum.map(fn item -> to_srtuct(item, module) end)
+  end
+
+  def to_srtuct(map, module) when is_map(map) and is_nil(module) do
+    map_with_atom_keys =
+      for {key, val} <- map, into: %{} do
+        {String.to_atom(key), to_srtuct(val, String.to_atom(key))}
+      end
+    moduleName = case module do
+      nil -> Response
+      :address -> Address
+      :geo -> Geo
+      :company -> Company
+      _ -> nil
+    end
+
+    struct(moduleName, map_with_atom_keys)
+  end
+
+
+  def to_srtuct(map, _module) do
+    map
+  end 
 end
